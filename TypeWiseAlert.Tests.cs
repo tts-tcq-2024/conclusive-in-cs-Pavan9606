1d6bb1159993c38211a77457db04d1ae99aca60b
Pavan9606 <118717558+Pavan9606@users.noreply.github.com>
2024-08-20 19:05:00 +0530

    Update TypeWiseAlert.Tests.cs

--- TypeWiseAlert.Tests.cs
+++ TypeWiseAlert.Tests.cs
@@ -3,6 +3,57 @@ using Xunit;
 
 public class TypeWiseAlertTests
 {
+   [Fact]
+    public void InferBreach_ShouldReturnCorrectBreachType(double temperature, double lowerLimit, double upperLimit, BreachType expectedBreach)
+    {
+        var result = TypewiseAlert.InferBreach(temperature, lowerLimit, upperLimit);
+        Assert.Equal(expectedBreach, result);
+    }
+  [Fact]
+  public void ClassifyTemperatureBreach_ShouldReturnCorrectBreachType(CoolingType coolingType, double temperature, BreachType expectedBreach)
+    {
+        ICoolingStrategy strategy = AlertFactory.CreateCoolingStrategy(coolingType);
+        var result = TypewiseAlert.ClassifyTemperatureBreach(strategy, temperature);
+        Assert.Equal(expectedBreach, result);
+    }
+  [Fact]
+  public void CheckAndAlert_ShouldTriggerCorrectAlert(AlertTarget alertTarget, CoolingType coolingType, double temperature, BreachType expectedBreach)
+    {
+        var batteryCharacter = new BatteryCharacter { coolingType = coolingType, brand = "BrandA" };
 
+        IAlert alert = alertTarget == AlertTarget.TO_CONTROLLER ? new ControllerAlertSpy() : new EmailAlertSpy() as IAlert;
 
+        TypewiseAlert.CheckAndAlert(alertTarget, batteryCharacter, temperature);
+
+        if (alert is ControllerAlertSpy controllerSpy)
+        {
+            Assert.Equal(expectedBreach, controllerSpy.TriggeredBreachType);
+        }
+        else if (alert is EmailAlertSpy emailSpy)
+        {
+            Assert.Equal(expectedBreach == BreachType.NORMAL, !emailSpy.AlertTriggered);
+        }
+    }
+   [Fact]
+  public void CreateAlert_ShouldReturnCorrectAlertInstance(AlertTarget alertTarget, Type expectedType)
+    {
+        var alert = AlertFactory.CreateAlert(alertTarget);
+        Assert.IsType(expectedType, alert);
+    }
+  [Fact]
+   public void CreateCoolingStrategy_ShouldReturnCorrectStrategyInstance(CoolingType coolingType, Type expectedType)
+    {
+        var strategy = AlertFactory.CreateCoolingStrategy(coolingType);
+        Assert.IsType(expectedType, strategy);
+    }
+   [Fact]
+  public void CreateAlert_ShouldThrowExceptionForInvalidAlertTarget()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => AlertFactory.CreateAlert((AlertTarget)999));
+    }
+   [Fact]
+  public void CreateCoolingStrategy_ShouldThrowExceptionForInvalidCoolingType()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => AlertFactory.CreateCoolingStrategy((CoolingType)999));
+    }
 }
