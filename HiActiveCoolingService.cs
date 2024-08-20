e59df5a9e2fe21d02714c33d1aa0ece12141a5b6
Pavan9606 <118717558+Pavan9606@users.noreply.github.com>
2024-08-20 18:51:34 +0530

    Create HiActiveCoolingService.cs

---
+++ HiActiveCoolingService.cs
@@ -0,0 +1,5 @@
+public class HiActiveCoolingService : ICoolingService
+{
+    public BreachType ClassifyTemperature(double temperatureInC) =>
+        TypewiseAlert.InferBreach(temperatureInC, 0, 45);
+}
