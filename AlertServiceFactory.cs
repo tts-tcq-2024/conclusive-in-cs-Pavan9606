4c6f435641a63f5cf4ed853bf6232d41c737ac0b
Pavan9606 <118717558+Pavan9606@users.noreply.github.com>
2024-08-20 18:46:28 +0530

    Update  AlertServiceFactory.cs

---
+++ AlertServiceFactory.cs
@@ -0,0 +1,12 @@
+public static class AlertServiceFactory
+{
+    public static IAlertService CreateAlertService(AlertTarget alertTarget)
+    {
+        return alertTarget switch
+        {
+            AlertTarget.TO_CONTROLLER => new ControllerAlertService(),
+            AlertTarget.TO_EMAIL => new EmailAlertService(),
+            _ => throw new ArgumentOutOfRangeException(nameof(alertTarget), alertTarget, null)
+        };
+    }
+}
