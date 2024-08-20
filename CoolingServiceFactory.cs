22602b83d8729ee0d4010d1baa300207b51ae744
Pavan9606 <118717558+Pavan9606@users.noreply.github.com>
2024-08-20 18:50:13 +0530

    UpdateCoolingServiceFactory.cs

---
+++ CoolingServiceFactory.cs
@@ -0,0 +1,13 @@
+public static class CoolingServiceFactory
+{
+    public static ICoolingService CreateCoolingService(CoolingType coolingType)
+    {
+        return coolingType switch
+        {
+            CoolingType.PASSIVE_COOLING => new PassiveCoolingService(),
+            CoolingType.HI_ACTIVE_COOLING => new HiActiveCoolingService(),
+            CoolingType.MED_ACTIVE_COOLING => new MedActiveCoolingService(),
+            _ => throw new ArgumentOutOfRangeException(nameof(coolingType), coolingType, null)
+        };
+    }
+}
