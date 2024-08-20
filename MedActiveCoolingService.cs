1ecbac11fbd6ffd842767aa7c3bf320786747a61
Pavan9606 <118717558+Pavan9606@users.noreply.github.com>
2024-08-20 18:52:57 +0530

    Create MedActiveCoolingService.cs

---
+++ MedActiveCoolingService.cs
@@ -0,0 +1,5 @@
+ public class MedActiveCoolingService : ICoolingService
+ {
+     public BreachType ClassifyTemperature(double temperatureInC) =>
+         TypewiseAlert.InferBreach(temperatureInC, 0, 40);
+ }
