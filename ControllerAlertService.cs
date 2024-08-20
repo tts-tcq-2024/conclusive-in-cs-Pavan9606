3727db2fee948c32fb89793ecf5dd7c807c5d001
Pavan9606 <118717558+Pavan9606@users.noreply.github.com>
2024-08-20 18:49:30 +0530

    Update  ControllerAlertService.cs

---
+++ ControllerAlertService.cs
@@ -0,0 +1,8 @@
+public class ControllerAlertService : IAlertService
+{
+    public void SendAlert(BreachType breachType)
+    {
+        const ushort header = 0xfeed;
+        Console.WriteLine($"0x{header:x} : {breachType}");
+    }
+}
