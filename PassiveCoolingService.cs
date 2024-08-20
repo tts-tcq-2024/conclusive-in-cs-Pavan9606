e291d09a290c43c22eaa05cb9427a178b5edeca8
Pavan9606 <118717558+Pavan9606@users.noreply.github.com>
2024-08-20 18:53:21 +0530

    Create PassiveCoolingService.cs

---
+++ PassiveCoolingService.cs
@@ -0,0 +1,5 @@
+public class PassiveCoolingService : ICoolingService
+{
+    public BreachType ClassifyTemperature(double temperatureInC) =>
+        TypewiseAlert.InferBreach(temperatureInC, 0, 35);
+}
