68902417579cf4d10f4abf4713ca6b13ab602f4e
Pavan9606 <118717558+Pavan9606@users.noreply.github.com>
2024-08-21 10:42:13 +0530

    Update MedActiveCoolingService.cs

--- MedActiveCoolingService.cs
+++ MedActiveCoolingService.cs
@@ -1,4 +1,5 @@
- public class MedActiveCoolingService : ICoolingService
+using System;
+public class MedActiveCoolingService : ICoolingService
  {
      public BreachType ClassifyTemperature(double temperatureInC) =>
          TypewiseAlert.InferBreach(temperatureInC, 0, 40);
